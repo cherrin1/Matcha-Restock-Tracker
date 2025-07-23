6ed3b2408805965e09483310f433c79de0f63a0d
cherrin1 <cherrin@connexpay.com>
2025-07-23 22:37:58 +0000

    hy

--- MatchaTracker/app/_layout.tsx
+++ MatchaTracker/app/_layout.tsx
@@ -1,32 +1,5 @@
 import { Stack } from 'expo-router';
-import * as Notifications from 'expo-notifications';
-import * as Device from 'expo-device';
-import { useEffect } from 'react';
-
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: true,
-    shouldSetBadge: false,
-  }),
-});
 
 export default function RootLayout() {
-  useEffect(() => {
-    registerForPushNotificationsAsync();
-  }, []);
-
-  const registerForPushNotificationsAsync = async () => {
-    if (Device.isDevice) {
-      const { status: existingStatus } = await Notifications.getPermissionsAsync();
-      let finalStatus = existingStatus;
-      
-      if (existingStatus !== 'granted') {
-        const { status } = await Notifications.requestPermissionsAsync();
-        finalStatus = status;
-      }
-    }
-  };
-
   return <Stack screenOptions={{ headerShown: false }} />;
 }
